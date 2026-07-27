ac9f95dea6dd029335971c0ad040125987bdd873
jacky4896 <hcwaaaa128@gmail.com>
2026-07-27 17:47:04 +0800

    更新关闭修复的离线缓存

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,11 @@
+const CACHE = 'my-ledger-v3';
+const ASSETS = ['./', './my-money.html', './manifest.webmanifest', './ledger-icon.svg'];
+
+self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
+self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
+self.addEventListener('fetch', event => {
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request)));
+});
 const CACHE = 'my-ledger-v2';
 const ASSETS = ['./', './my-money.html', './manifest.webmanifest', './ledger-icon.svg'];
 
